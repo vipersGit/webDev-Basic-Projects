25c29e76aa539f597180d1b747e20c504fd23823
vipersGit <jiteshsaini6396@gmail.com>
2026-09-10 14:32:56 +0530

    v-2

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,49 +1,76 @@
-const express = require("express");
-const app = express();
+// REQUIRED PACKAGES
 const cors = require("cors");
+const express = require("express");
+const noteModel = require("./models/notes.model");
 const path = require("path");
-app.use(cors());
+
+// EXECUTABLE PACKAGES
+const app = express();
+
+
+// MIDDLE-WARES
 app.use(express.json());
+app.use(cors());
 app.use(express.static("./public"));
-const noteModel = require("./models/note.model");
 
-app.get("/api/notes", async (req, res) => {
-  const notes = await noteModel.find();
-  res.status(200).json({
-    msg: "notes were fetched",
-    notes: notes,
-  });
-});
 
-app.post("/api/notes", async (req, res) => {
-  const { title, description } = req.body;
-  const note = await noteModel.create({
-    title,
-    description,
-  });
-  res.status(201).json({
-    msg: "a note was created",
-    note: note,
-  });
-});
+// REST APIs
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find();
 
-app.patch("/api/notes/:id", async (req, res) => {
-  const { title } = req.body;
-  await noteModel.findByIdAndUpdate(req.params.id, { title });
-  res.status(200).json({
-    msg: "a note was partially updated",
-  });
-});
+    res.status(200).json({
+        msg: "notes were fetched",
+        notes: notes
+    })
+})
 
-app.delete("/api/notes/:id", async (req, res) => {
-  await noteModel.findByIdAndDelete(req.params.id);
-  res.status(204).json({});
-});
+app.post("/notes", async (req, res) => {
+    const { name, gender, age } = req.body;
+    const note = await noteModel.create({
+        name, gender, age
+    })
+    res.status(201).json({
+        msg: "a note was created",
+        note: note
+    })
+})
 
-app.use("*name",(req,res)=>{
-  res.sendFile(path.join(__dirname, "..", "/public/index.html"))
+app.patch("/notes/:id", async (req, res) => {
+    const { name } = req.body;
+    const note = await noteModel.findByIdAndUpdate(req.params.id, {
+        name
+    })
+    res.status(200).json({
+        msg: "a note was partially updated",
+        note: note
+    })
+
+})
+
+app.put("/notes/:id", async (req, res) => {
+    const { name, gender, age } = req.body;
+    const note = await noteModel.findByIdAndUpdate(req.params.id, {
+        name, gender, age
+    })
+    res.status(200).json({
+        msg: "a note was completely updated",
+        note: note
+    })
+})
+
+app.delete("/notes/:id", async (req, res) => {
+    await noteModel.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+        msg: "a note was deleted",
+    })
+})
+
+
+app.use("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "/public/index.html"))
 })
 
 
 module.exports = app;
+
 
