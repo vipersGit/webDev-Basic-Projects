f8132d9d6c1d687cf45ed12729961185549bd311
vipersGit <jiteshsaini6396@gmail.com>
2026-09-10 14:12:41 +0530

    del

--- back-end/src/models/note.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const noteSchema = new mongoose.Schema({
-    title: String,
-    description: String
-});
-
-const noteModel = mongoose.model("Note", noteSchema);
-module.exports = noteModel;
